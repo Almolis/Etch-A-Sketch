cffab2faac629606ac2a42acafa00b928efb2001
Sandra Almoli <almolisandra@gmail.com>
2021-03-29 13:27:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+function createBoard (rows, columns) {
+    let table = document.querySelector(".table"); 
+    for (let i =1; i <= rows; i++) {
+        let tr = document.createElement("tr"); 
+        table.append(tr); 
+        for (let j=1; j <= columns; j++) {
+            let td = document.createElement("td");  
+            table.append(td); 
+        }
+    } return table
+}
+
+createBoard(100, 100)
+
+
+
+let td = document.querySelectorAll("td");
+
+td.forEach(element => {
+        element.addEventListener("mouseover", setColor);
+      });
+      
+function setColor(){
+        this.style.background = "grey";
+      }
+
+
+
+ 
