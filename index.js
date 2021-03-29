000008097ebde98a6f3655da4a1751177c2594a8
Sandra Almoli <almolisandra@gmail.com>
2021-03-29 14:48:37 +0200

    reset

--- index.js
+++ index.js
@@ -16,15 +16,21 @@ createBoard(100, 150)
 
 
 let td = document.querySelectorAll("td");
-
 td.forEach(element => {
         element.addEventListener("mouseover", setColor);
-      });
-      
+      });      
 function setColor(){
         this.style.background = "grey";
       }
 
+//let children = document.querySelector(".table").children;
+let button = document.querySelector("button")
 
+button.addEventListener("click", resetTable)
 
- 
+function resetTable () {
+    let children = document.querySelector(".table").children;
+    for (let i=0; i<=children.length; i++){
+        children[i].style.backgroundColor = "white"; 
+    } 
+}
